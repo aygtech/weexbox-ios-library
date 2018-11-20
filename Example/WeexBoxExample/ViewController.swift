c7529abd1b293e5953e40f0d30ad3e493d3f8bd6
Mario <mario.huang@aiyuangong.com>
2018-11-20 18:09:22 +0800

    spec.static_framework = true

---
+++ Example/WeexBoxExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeexBoxExample
+//
+//  Created by Mario on 2018/11/20.
+//  Copyright © 2018 WeexBox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
