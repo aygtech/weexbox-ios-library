c182895d24810be7c8e293cb40d78fad014a4c10
Mario <myeveryheart@qq.com>
2018-12-21 10:09:53 +0800

    判断title

--- WeexBox/Controller/WBBaseViewController.swift
+++ WeexBox/Controller/WBBaseViewController.swift
@@ -21,7 +21,9 @@ import Foundation
         
         rt_disableInteractivePop = router.disableGestureBack
         view.backgroundColor = .white
-        title = router.title
+        if title == nil {
+            title = router.title
+        }
         navigationController?.isNavigationBarHidden = router.navBarHidden
     }
     
